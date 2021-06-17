920b705d7df2b9a970eea7fc6a9711b4b96aa340
dengzemiao <dengzemiaovip@163.com>
2021-06-17 19:33:47 +0800

    antdv 中文化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <div id="app" class="container-full">
-    <router-view/>
-  </div>
+  <a-config-provider :locale="zhCN">
+    <div id="app" class="container-full">
+      <router-view/>
+    </div>
+  </a-config-provider>
 </template>
+
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+export default {
+  data () {
+    return {
+      zhCN
+    }
+  }
+}
+</script>
 
 <style>
 </style>
