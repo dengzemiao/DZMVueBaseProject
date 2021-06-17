87285a9bdda6558171c8761d1f4c784bc11931ce
dengzemiao <dengzemiaovip@163.com>
2021-06-17 19:27:35 +0800

    添加moment

--- src/utils/use.js
+++ src/utils/use.js
@@ -9,6 +9,11 @@ import Pub from '@/utils/public'
 // Vue 存储管理插件
 import VueLs from 'vue-ls'
 
+// 日期工具
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
+
 // 配置到 Vue
 Vue.use(VueLs, Pub.VUE_LS_OPTIONS)
 Vue.prototype.$pub = Pub
