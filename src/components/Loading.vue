59d55c7d2aa6d3b1687b6dceb4915bc79a35fc9e
dengzemiao <dengzemiaovip@163.com>
2021-07-07 14:09:28 +0800

    .

---
+++ src/components/Loading.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="loading" :class="loading ? 'loading loading-show' : 'loading'">
+    <van-loading class="loading-content" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: () => false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loading {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  z-index: 200;
+  background: rgba(255, 255, 255, 0.45);
+  opacity: 0;
+  transition: all 0.25s;
+}
+.loading-show {
+  opacity: 1;
+}
+.loading-content {
+  color: #1989fa;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,48 @@
     <!-- 提示 -->
     <span>BASE PROJECT</span>
     <span>Vue 2.x</span>
+    <!-- 加载组件 -->
+    <Loading :loading="isLoading"></Loading>
     <!-- 按钮 -->
     <van-button type="primary" style="margin-top: 15px;">Vant UI 已安装</van-button>
   </div>
 </template>
 
 <script>
+import Loading from '@/components/Loading'
+import Upload from '@/components/Upload'
 export default {
+  components: {
+    Loading,
+    Upload
+  },
+  data () {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    // 准备上传
+    beforeUploadPro () {
+      this.isLoading = true
+      return true
+    },
+    // 上传结果
+    uploadResult (fileJson, res) {
+      // 上传中信息输出
+      console.log(fileJson, res)
+      // 进入加载
+      this.isLoading = false
+      // 上传结果判断
+      if (fileJson.status === 'done') {
+        this.$message.success('上传成功')
+      } else if (fileJson.status === 'error') {
+        this.$message.error('上传失败')
+      } else {
+        // console.log('正在上传');
+      }
+    }
+  }
 }
 </script>
 
