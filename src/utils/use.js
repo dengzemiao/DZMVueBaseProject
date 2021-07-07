be4565aeef889722476ca4e1a1a4e7df8b33ad6d
dengzemiao <dengzemiaovip@163.com>
2021-07-07 12:05:30 +0800

    .

--- src/utils/use.js
+++ src/utils/use.js
@@ -9,6 +9,10 @@ import Pub from '@/utils/public'
 // axios
 import { VueAxios } from '@/api/axios'
 
+// mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
 // Vue 存储管理插件
 import VueLs from 'vue-ls'
 
@@ -19,6 +23,7 @@ moment.locale('zh-cn')
 
 // 配置到 Vue
 Vue.use(VueAxios)
+Vue.use(MintUI)
 Vue.use(VueLs, Pub.VUE_LS_OPTIONS)
 Vue.prototype.$pub = Pub
 Vue.prototype.$moment = moment
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container-full-column-center">
+    <!-- 提示 -->
     <span>BASE PROJECT</span>
     <span>Vue 2.x</span>
+    <!-- 按钮 -->
+    <mt-button type="primary" style="margin-top: 15px;">Mint UI 已安装</mt-button>
   </div>
 </template>
 
