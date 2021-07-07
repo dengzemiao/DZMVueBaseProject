af7e101fa23869ad5f94c65ab6949a39a1803b74
dengzemiao <dengzemiaovip@163.com>
2021-07-07 11:53:10 +0800

    .

--- src/utils/use.js
+++ src/utils/use.js
@@ -6,6 +6,9 @@ import '@/assets/css/common.css'
 // 公用属性对象
 import Pub from '@/utils/public'
 
+// axios
+import { VueAxios } from '@/api/axios'
+
 // Vue 存储管理插件
 import VueLs from 'vue-ls'
 
@@ -15,6 +18,7 @@ import 'moment/locale/zh-cn'
 moment.locale('zh-cn')
 
 // 配置到 Vue
+Vue.use(VueAxios)
 Vue.use(VueLs, Pub.VUE_LS_OPTIONS)
 Vue.prototype.$pub = Pub
 Vue.prototype.$moment = moment
