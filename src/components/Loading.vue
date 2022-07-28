c26e17e3075a97175fbcfeaa1f3e3cd7d0d66278
dengzemiao <dengzemiaovip@163.com>
2022-07-28 15:16:28 +0800

    2022-07-28 15:16:28 提交优化

---
+++ src/components/Loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="loading" :class="loading ? 'loading loading-show' : 'loading'">
+    <a-spin class="loading-content" :size="size" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 加载状态
+    loading: {
+      type: Boolean,
+      default: () => false
+    },
+    // Icon大小：small、default、large
+    size: {
+      type: String,
+      default: () => 'default'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loading {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  z-index: 200;
+  background: rgba(255, 255, 255, 0.45);
+  opacity: 0;
+  transition: all 0.25s;
+}
+.loading-show {
+  opacity: 1;
+}
+.loading-content {
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="container-full-column-center">
+    <!-- 提示 -->
     <span>BASE PROJECT</span>
     <span>Vue 3.x</span>
-    <a-date-picker />
+    <!-- 安装提示 -->
+    <a-alert message="Antdv 已安装" style="margin-top: 15px;" type="success" show-icon />    <!-- 加载组件 -->
+    <!-- 加载组件 -->
+    <Loading :loading="isLoading"></Loading>
+    <!-- 上传组件 -->
+    <Upload :beforeUploadPro="beforeUploadPro" :uploadResult="uploadResult"></Upload>
   </div>
 </template>
 
 <script>
+import Loading from '@/components/Loading'
+import Upload from '@/components/Upload'
 export default {
-  mounted () {
+  components: {
+    Loading,
+    Upload
+  },
+  data () {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    // 准备上传
+    beforeUploadPro () {
+      this.isLoading = true
+      return true
+    },
+    // 上传结果
+    uploadResult (fileJson, res) {
+      // 上传中信息输出
+      console.log(fileJson, res)
+      // 进入加载
+      this.isLoading = false
+      // 上传结果判断
+      if (fileJson.status === 'done') {
+        this.$message.success('上传成功')
+      } else if (fileJson.status === 'error') {
+        this.$message.error('上传失败')
+      } else {
+        // console.log('正在上传');
+      }
+    }
   }
 }
 </script>
