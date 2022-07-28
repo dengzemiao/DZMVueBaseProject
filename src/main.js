16772aa93e35e3d00e6271a2d63e53e5d8df1d5c
dengzemiao <dengzemiaovip@163.com>
2022-07-28 15:01:01 +0800

    2022-07-28 15:01:01 提交优化

--- src/main.js
+++ src/main.js
@@ -22,16 +22,11 @@ moment.locale('zh-cn')
 // 创建对象
 const app = createApp(App)
 // 使用并挂载
-app
-.use(store)
-.use(router)
-.mount('#app')
-
+app.use(store).use(router).mount('#app')
 // 必须使用 nextTick，不然会有加载顺序问题，导致绑定失败
 nextTick(() => {
   app.config.globalProperties.$pub = Pub
   app.config.globalProperties.$moment = moment
 })
-
 // 导出
 // export default app
