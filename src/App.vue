1025edb1b7845f16cce8a1e1873580c6b37535ee
dengzemiao <dengzemiaovip@163.com>
2025-11-14 13:35:00 +0800

    2025-11-14 13:34:55 提交优化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-   <RouterView />
-</template>
-
-<script setup>
-</script>
-
-<style scoped></style>
+   <a-config-provider :locale="locale">
+    <RouterView />
+   </a-config-provider>
+ </template>
+ 
+ <script setup>
+ // 导入中文语言包
+ import zhCN from 'ant-design-vue/es/locale/zh_CN';
+ // 设置语言包
+ const locale = zhCN;
+ </script>
+ 
+ <style scoped></style>
+ 
